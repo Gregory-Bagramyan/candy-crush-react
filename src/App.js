692534baf4a6f59b23e4c6e3bb331f1ecd48df04
Gregory <85247790+Gregory-Bagramyan@users.noreply.github.com>
2023-01-13 23:29:31 +0100

    added check col of 4, row of 3 and row of 4

--- src/App.js
+++ src/App.js
@@ -23,6 +23,60 @@ const App = () => {
     }
   };
 
+  const checkForColumnOfFour = () => {
+    for (let i = 0; i < 39; i++) {
+      const columnOfFour = [i, i + width, i + width * 2, i + width * 3];
+      const decidedColor = currentColorArrangment[i];
+
+      if (
+        columnOfFour.every(
+          (square) => currentColorArrangment[square] === decidedColor
+        )
+      ) {
+        columnOfFour.forEach((square) => (currentColorArrangment[square] = ""));
+      }
+    }
+  };
+
+  const checkForRowOfThree = () => {
+    for (let i = 0; i < 64; i++) {
+      const rowOfThree = [i, i + 1, i + 2];
+      const decidedColor = currentColorArrangment[i];
+      const notValid = [
+        6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64,
+      ];
+
+      if (notValid.includes(i)) continue;
+      if (
+        rowOfThree.every(
+          (square) => currentColorArrangment[square] === decidedColor
+        )
+      ) {
+        rowOfThree.forEach((square) => (currentColorArrangment[square] = ""));
+      }
+    }
+  };
+
+  const checkForRowOfFour = () => {
+    for (let i = 0; i < 64; i++) {
+      const rowOfFour = [i, i + 1, i + 2, i + 3];
+      const decidedColor = currentColorArrangment[i];
+      const notValid = [
+        5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53,
+        54, 55, 62, 63, 64,
+      ];
+
+      if (notValid.includes(i)) continue;
+      if (
+        rowOfFour.every(
+          (square) => currentColorArrangment[square] === decidedColor
+        )
+      ) {
+        rowOfFour.forEach((square) => (currentColorArrangment[square] = ""));
+      }
+    }
+  };
+
   const createBoard = () => {
     const randomColorArrangment = [];
     for (let i = 0; i < width * width; i++) {
@@ -40,11 +94,20 @@ const App = () => {
 
   useEffect(() => {
     const timer = setInterval(() => {
+      checkForColumnOfFour();
+      checkForRowOfFour();
       checkForColumnOfThree();
+      checkForRowOfThree();
       setCurrentColorArrangment([...currentColorArrangment]);
     }, 100);
     return () => clearInterval(timer);
-  }, [checkForColumnOfThree, currentColorArrangment]);
+  }, [
+    checkForColumnOfFour,
+    checkForRowOfFour,
+    checkForColumnOfThree,
+    checkForRowOfThree,
+    currentColorArrangment,
+  ]);
 
   console.log(currentColorArrangment);
 
